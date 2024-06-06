5ca7970b8d3f49a0e33a646b636d39d457708937
SreylenHuon <hounsreylen53@gamil.com>
2024-06-07 00:24:45 +0700

    first commit

---
+++ vite-project/src/components/Button.jsx
@@ -0,0 +1,12 @@
+export function Button(){
+    return <button style={btnStyle}>Click me!</button>;
+
+}
+ const btnStyle = {
+    color: "white",
+    padding: "5px",
+    backgroundColor: "blue",
+    border: "none",
+    bordeerradius: "4px",
+    margin: "10px"
+ };
